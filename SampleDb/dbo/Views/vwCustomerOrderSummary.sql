3411ff5ecd66cb75d4091d393e59dafd9c86bd1f
Wie <wietherton@live.co.uk>
2014-12-17 22:40:26 +0000

    SampleDb

---
+++ SampleDb/dbo/Views/vwCustomerOrderSummary.sql
@@ -0,0 +1,11 @@
+﻿
+-- Create a handy view summarizing customer orders
+CREATE VIEW vwCustomerOrderSummary WITH SCHEMABINDING AS
+ SELECT
+   c.CustomerID, c.FirstName, c.LastName --, c.CustomerRanking,
+   ,ISNULL(SUM(oh.OrderTotal), 0) AS OrderTotal
+  FROM
+   dbo.Customer AS c
+   LEFT OUTER JOIN dbo.OrderHeader AS oh ON c.CustomerID = oh.CustomerID
+  GROUP BY
+   c.CustomerID, c.FirstName, c.LastName-- , c.CustomerRanking
